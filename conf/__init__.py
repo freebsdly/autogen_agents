410e2596db9eab8135b9e28e018ab229bbd3cea1
qinhuajun <qinhuajun@outlook.com>
2025-12-07 20:56:20 +0800

    init project

---
+++ conf/__init__.py
@@ -0,0 +1,11 @@
+"""
+conf package initializer.
+
+This module imports and exposes the `settings` object from the config module.
+"""
+
+from .config import settings
+
+__all__ = [
+    "settings",
+]
---
+++ conf/config.py
@@ -0,0 +1,12 @@
+
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    envvar_prefix="DYNACONF",
+    settings_files=['settings.toml', '.secrets.toml'],
+    merge_enabled=True,
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
+# `merge_enabled` = Enable merging of nested configurations from multiple files.
